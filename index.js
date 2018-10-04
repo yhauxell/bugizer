b11c68d1e078813d0c7130b64c4f4e4a0f26a163
Yausell Ruiz <yhauxell@gmail.com>
2018-10-04 09:55:12 -0300

    Initial adding basic widget structure

---
+++ index.js
@@ -0,0 +1,29 @@
+var BugReporter = (function(props){
+    var def = {
+        position:'right-bottom'
+    }
+
+    var load = function(){
+        console.log("Load");
+    }
+
+    var render = function(){
+
+    }
+
+    var send = function(){
+
+    }
+
+    var enableScreenshot = function(){
+
+    }
+
+    return {
+        load: load,
+        render:render,
+        send:send,
+        enableScreenshot: enableScreenshot
+    }
+
+})();
